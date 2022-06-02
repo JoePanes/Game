a0a7d480a44ac1021d65513476b26a9adc78f63e
JPanes <joepanes@outlook.com>
2022-06-02 16:17:44 +0100

    Implemented Sigil object
    Created the Sigil object and associated functionality for destroying enemy objects.

---
+++ Assets/Scripts/Player/Sigil.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sigil : MonoBehaviour
+{
+
+    public GameObject SigilLight;
+    public ParticleSystem SigilParticles;
+
+    private float speed = -3f;
+
+    private bool hasBeenTriggered;
+    private bool animationComplete;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animationComplete = false;
+        hasBeenTriggered = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hasBeenTriggered)
+        {
+            ShowSigilTriggered();
+        }
+    }
+
+    void ShowSigilTriggered()
+    {
+        SigilLight.transform.Translate(Vector3.down * speed * Time.deltaTime);
+
+        // After sending the light up, bring it back below the floor
+        if (SigilLight.transform.position.y > 1.5f)
+        {
+            speed = -speed;
+            animationComplete = true;
+        }
+
+        // Destroy the sigil when the animation for activation is complete 
+        if (SigilLight.transform.position.y < 0f && animationComplete)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Enemy"))
+        {
+            hasBeenTriggered = true;
+            SigilParticles.Play();
+            Destroy(other.gameObject);
+
+        }
+    }
+
+}
